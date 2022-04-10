cac171fdbfd1baf3870e7327f0c91be67675eca3
Perligates <kera.mahmood@dcmail.ca>
2022-04-10 19:54:13 -0400

    Add portfolio content and modals

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,8 +4,28 @@
 /*	CUSTOM JS/JQUERY SCRIPTS
 /*===================================================================================*/
 
-function pauseVideo() {
-    document.getElementById('demo-reel').pause();
-}
+$('#modal').on('hide.bs.modal', function () {
+    $video = $(this).find('video')[0].pause();
+});
 
+$('#modal').on('show.bs.modal', function (e) {
+    
+    videoLink = $(e.relatedTarget).data('link');
+    $video = $(this).find('video')[0];
 
+    if (videoLink != $video.src)
+    {
+        $video.src = videoLink;
+        $video.load();
+    }
+});
+
+$('#modal-img').on('show.bs.modal', function (e) {
+    
+    imgLink = $(e.relatedTarget).data('link');
+    $img = $(this).find('img')[0];
+
+    if (imgLink != $img.src)
+        $img.src = imgLink;
+        
+});
